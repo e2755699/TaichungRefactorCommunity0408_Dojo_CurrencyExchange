12d9e3427e45264788e56126d614b44a7cf6aa32
dustin <dustin.liu@titansoft.com.sg>
2021-04-07 08:14:26 +0800

    init

---
+++ KentBeckTDDCunrrecnyExchange_20210407/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace KentBeckTDDCunrrecnyExchange_20210407
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
